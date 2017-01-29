0f6a64d4417728614fc7ab77cf9909f55f4837e5
Stephen Brimhall <stephen.brimhall@icloud.com>
2017-01-29 08:22:11 -0800

    Added input bar class (not functional yet) and fixed bugs in messagelog

---
+++ src/inputbar.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#
+# inputbar.py
+# Created by Stephen Brimhall on 1/28/17
+# Copyright (c) SST CTF and Stephen Brimhall 2017.
+#
+# Controls the message input bar of the Communicator chatd client
+#
+
+# Begin system imports
+import curses
+# End system imports
+
+class InputBar:
+    # Controls user input into the program
+
+    pass;
